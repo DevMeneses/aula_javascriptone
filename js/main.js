04864c9a910d2794454ee51a23157687cd45f0ed
matheus457 <menesesmatheus993@gmail.com>
2020-06-24 21:38:40 -0300

    primeiro commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,47 @@
+var d = new Date();
+alert(d);
+
+
+
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+
+var idade = prompt("Qual sua idade");
+
+if(idade >= 18){
+  alert("Maior de idade");
+
+}else{
+alert("Menor de idade");
+}
+
+var fruta = {nome:"maça",cor:"vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor);
+
+var lista =["maça", "pêra", "laranja"];
+lista.push("uva");
+//lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" = "));
 
 var nome = "Matheus Meneses";
-var idade = 19;
-alert(nome + " tem " + idade + " anos");
+var n1 = 5;
+var n2 = 3;
+var frase = "japão é o melhor time do mundo";
+alert (nome + "tem" + idade + "anos");
+alert (idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
